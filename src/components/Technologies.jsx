351d5a6323fdf6dc26360f7e4f8e577fdbaf50f4
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-08-16 11:39:07 +0530

    threads added & chakra ui

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,6 +1,7 @@
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { SiJavascript } from "react-icons/si";
+import { SiChakraui } from "react-icons/si";
 import { RiReactjsLine } from "react-icons/ri";
 import { SiMongodb } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
@@ -28,105 +29,126 @@ export default function Technologies() {
   return (
     <div className="border-b border-neutral-800 pb-24">
       <motion.h1
-       whileInView={{opacity:1, x:0}}
-       initial={{opacity:0,x:100}}
-       transition={{duration: 1.5}}
-      className="my-20 text-center text-4xl">Technologies</motion.h1>
-      <motion.div 
-      whileInView={{opacity:1, x:0}}
-      initial={{opacity:0,x:-100}}
-      transition={{duration: 1.5}}
-      
-      className="flex flex-wrap items-center justify-center gap-4">
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
         <motion.div
-        variants={iconVariants(2.5)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaHtml5 className="text-7xl text-orange-500" />
         </motion.div>
 
-        <motion.div 
-        variants={iconVariants(3)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaCss3Alt className="text-7xl text-indigo-600" />
         </motion.div>
 
-        <motion.div 
-        variants={iconVariants(5)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiJavascript className="text-7xl text-yellow-400" />
         </motion.div>
 
         <motion.div
-        variants={iconVariants(3.5)}
-        initial="initial"
-        animate="animate"
-        
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiReactjsLine className="text-7xl text-cyan-400" />
         </motion.div>
 
         <motion.div
-        variants={iconVariants(1.5)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+          variants={iconVariants(1.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiMongodb className="text-7xl text-green-400" />
         </motion.div>
 
         <motion.div
-        variants={iconVariants(6)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiMongoose className="text-7xl text-red-400" />
         </motion.div>
 
-        <motion.div 
-        variants={iconVariants(4.5)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(4.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaNodeJs className="text-7xl text-green-700" />
         </motion.div>
 
         <motion.div
-        variants={iconVariants(2.5)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiExpress className="text-7xl text-white" />
         </motion.div>
-        
+
         <motion.div
-        variants={iconVariants(7)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+          variants={iconVariants(7)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaBootstrap className="text-7xl text-violet-500" />
         </motion.div>
 
-        <motion.div 
-        variants={iconVariants(5.5)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(5.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiTailwindCssFill className="text-7xl text-orange-600" />
         </motion.div>
 
-        <motion.div 
-        variants={iconVariants(6.5)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(6.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiMysql className="text-7xl text-red-700" />
         </motion.div>
 
+        <motion.div
+          variants={iconVariants(8)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiChakraui className="text-7xl text-blue-700" />
+        </motion.div>
       </motion.div>
     </div>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,8 @@
- 
 import project1 from "../assets/projects/project1.png";
 import project2 from "../assets/projects/project2.png";
 import project3 from "../assets/projects/project3.png";
 import project4 from "../assets/projects/project4.png";
- 
+import project5 from "../assets/projects/project4.png";
 
 export const HERO_CONTENT = `I am a passionate full-stack developer (MERN stack) with a knack for crafting robust and scalable web applications. I am a certified Guvi (IITM) developer with hands-on experience in project-based curriculum`;
 
@@ -15,57 +14,85 @@ export const EXPERIENCES = [
     role: "Mern Stack Developer",
     company: "Guvi",
     description: ` After working in the mechanical field for 2 years, I began exploring how the web works behind the scenes, igniting my passion for web development. I made the decision to join Guvi's project-based curriculum, and I successfully completed the program, earning certification `,
-    technologies: ["Html", "Css", "Js","React.js","Node.js","Express.js","MongoDB","MySql"],
+    technologies: [
+      "Html",
+      "Css",
+      "Js",
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "MySql",
+    ],
   },
   {
     year: "2020 - 2022",
     role: "Mechanical Engineer",
     company: "TVS",
-    description: `non destructive tester for the various parts of the vehicle to ensuring the quality and managing the number of jobs exporting day by day` ,  technologies: [ "ultrasonic testing (UT)", "radiographic testing (RT)", "magnetic particle testing (MPT)" ]
+    description: `non destructive tester for the various parts of the vehicle to ensuring the quality and managing the number of jobs exporting day by day`,
+    technologies: [
+      "ultrasonic testing (UT)",
+      "radiographic testing (RT)",
+      "magnetic particle testing (MPT)",
+    ],
   },
 ];
 
 export const PROJECTS = [
+  {
+    title: "Threads",
+    image: project5,
+    url: "https://threads-y867.onrender.com",
+    githubUrl: "https://github.com/Madhan235/threads",
+    description:
+      "A Social media site , where user can , post images, get likes and comments from other users, follow others, get followers, chat with other user with real time communication implemented with socket.io and much more to come",
+    technologies: ["MongoDB", "Express.js", "React.js", "Node.js", "chakraUI"],
+  },
 
   {
-    title:"Madhan Estate",
+    title: "Madhan Estate",
     image: project4,
-    url:"https://mern-estate-tvvm.onrender.com/",
-    githubUrl:"https://github.com/Madhan235/mern-estate",
-    description:"Madhan Estate is a full-stack real estate web application designed to streamline the property buying, selling, and renting process. Built using the MERN stack, this application offers a seamless search and efficient platform for users to browse, list, and manage real estate properties. The application integrates Firebase for secure OAuth authentication, providing a smooth and secure user login experience.",
-    technologies:["MongoDB", "Express.js", "React.js", "Node.js", "Tailwind.css"]
+    url: "https://mern-estate-tvvm.onrender.com/",
+    githubUrl: "https://github.com/Madhan235/mern-estate",
+    description:
+      "Madhan Estate is a full-stack real estate web application designed to streamline the property buying, selling, and renting process. Built using the MERN stack, this application offers a seamless search and efficient platform for users to browse, list, and manage real estate properties. The application integrates Firebase for secure OAuth authentication, providing a smooth and secure user login experience.",
+    technologies: [
+      "MongoDB",
+      "Express.js",
+      "React.js",
+      "Node.js",
+      "Tailwind.css",
+    ],
   },
   {
     title: "Blog Website",
     image: project1,
-    url:"https://mern-blog-8icy.onrender.com/",
-    githubUrl:"https://github.com/Madhan235/mern-blog",
+    url: "https://mern-blog-8icy.onrender.com/",
+    githubUrl: "https://github.com/Madhan235/mern-blog",
     description:
-    
-    "A role-based accessible page for admins, preferred themes with light and dark modes, and a blog post website for users with features such as adding posts, comments, and likes to comments. Additionally, user authentication with OAuth (Google) including the option to change profile pictures.",
-    technologies: [ "React", "Node.js", "MongoDB","Flow Bite","Firebase"],
+      "A role-based accessible page for admins, preferred themes with light and dark modes, and a blog post website for users with features such as adding posts, comments, and likes to comments. Additionally, user authentication with OAuth (Google) including the option to change profile pictures.",
+    technologies: ["React", "Node.js", "MongoDB", "Flow Bite", "Firebase"],
   },
   {
     title: "Chat App",
     image: project2,
-    url:"https://chat-app-2vp8.onrender.com/login",
-    githubUrl:"https://github.com/Madhan235/chat-app",
+    url: "https://chat-app-2vp8.onrender.com/login",
+    githubUrl: "https://github.com/Madhan235/chat-app",
     description:
-    "An application for chatting with all users implemented using socket.io with the MERN stack. When a user receives a notification online, a notification sound will play.",
-    technologies: [ "React", "Node.js", "MongoDB","Tailwind"],
+      "An application for chatting with all users implemented using socket.io with the MERN stack. When a user receives a notification online, a notification sound will play.",
+    technologies: ["React", "Node.js", "MongoDB", "Tailwind"],
   },
   {
     title: "Article Website",
     image: project3,
-    url:"https://catalog23.netlify.app/home_about_oil_price",
-    githubUrl:"https://github.com/Madhan235/items-server",
+    url: "https://catalog23.netlify.app/home_about_oil_price",
+    githubUrl: "https://github.com/Madhan235/items-server",
     description:
-    "A social awareness website implemented with Chart.js, focusing on mobile addiction among kids, drug addiction, and addiction to junk and sugary foods.",
-    technologies: [ "React", "Node.js", "MongoDB","Css","Bootstrap"],
+      "A social awareness website implemented with Chart.js, focusing on mobile addiction among kids, drug addiction, and addiction to junk and sugary foods.",
+    technologies: ["React", "Node.js", "MongoDB", "Css", "Bootstrap"],
   },
 ];
 
 export const CONTACT = {
- 
   email: "madhanofficialmern@gmail.com",
 };
